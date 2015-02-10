146848606c31e9f3d06b1131669346219453e7af
Leonardo Balter <leonardo.balter@gmail.com>
2015-02-10 17:14:56 -0500

    Update dependencies

--- Gruntfile.js
+++ Gruntfile.js
@@ -202,7 +202,6 @@ module.exports = function(grunt) {
     // These plugins provide necessary tasks.
     grunt.loadNpmTasks("grunt-contrib-jshint");
     grunt.loadNpmTasks("grunt-contrib-uglify");
-    grunt.loadNpmTasks("grunt-contrib-copy");
     grunt.loadNpmTasks("grunt-contrib-concat");
     grunt.loadNpmTasks("grunt-contrib-clean");
     grunt.loadNpmTasks("grunt-contrib-qunit");
