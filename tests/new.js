d92a5d79dc499878f3680973692e7f84b872d81b
Leonardo Balter <leonardo.balter@gmail.com>
2015-02-20 17:02:04 -0500

    Update new tests

--- tests/new.js
+++ tests/new.js
@@ -1,18 +1,28 @@
-QUnit.module("Basics", {
+QUnit.module("initialization", {
     beforeEach: function() {
         this.element = $("#test-element");
     }
 });
 
-QUnit.test("simple usage", function(assert) {
+QUnit.test("basic", function(assert) {
     this.element.Arte();
-    assert.strictEqual(this.element.html(),
-        "<div class=\"\" style=\"min-height: 200px; height: inherit;\" contenteditable=\"true\">Please enter text ...</div>");
+    assert.ok(this.element.prop("contenteditable"), "turns into a contenteditable item");
+    assert.strictEqual(this.element.text(), "Please enter text ...", "set's default text");
+    assert.strictEqual(this.element.height(), 200, "set's basic height");
 });
 
-QUnit.test("ArteJS returns something", function(assert) {
-    var object = this.element.Arte();
+QUnit.test("no element", function(assert) {
+    var result = $("#i-dont-exist").Arte();
+    assert.equal(result, false, "an empty jQuery object doesn't create an Arte object");
+});
 
-    // Breaking
-    assert.ok(object instanceof Arte, "Object is an instance of Arte");
+QUnit.test("multiple elements", function(assert) {
+    var multiple = $("<div/><div/>").addClass("foo");
+
+    multiple.Arte();
+
+    assert.strictEqual(multiple.first().html(),
+        "<div class=\"\" style=\"min-height: 200px; height: inherit;\" contenteditable=\"true\">Please enter text ...</div>",
+        "only the first element is affected");
+    assert.strictEqual(multiple.last().html(), "", "the last element is not affected");
 });
