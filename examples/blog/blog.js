84cc87146340f1bbc809b50b9b409636d0196e96
Chris Kwan <chriswkwan@gmail.com>
2015-02-27 18:45:27 -0500

    Add some initial styling

--- examples/blog/blog.js
+++ examples/blog/blog.js
@@ -1,8 +1,26 @@
 (function(){
-	$(".editor").Arte();
+	// Initialize and get a reference to the editor
+	var $arte = $(".editor").Arte({
+		editorType: $.Arte.constants.editorTypes.richText,
+		styles: { // default styles can be passed in as well
+			height: "200px",
+			width: "300px",
+			overflow: "auto",
+			border: "1px dashed gray"
+		}
+	});
 
-	var buttons = ["bold"]
+	// Initialize the toolbar
+	var buttons = ["bold", "italic", "underline"];
+
+	$.Arte.Toolbar.configuration.requireEditorFocus = false; //cwkTODO not sure what this should be
+
 	$(".toolbar").ArteToolbar({
 		buttons: buttons
+		//requireEditorFocus: false
 	});
+
+	// Set focus to the editor so the toolbar becomes enabled
+	$arte.focus();
+
 })();
